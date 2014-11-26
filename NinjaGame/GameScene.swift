b134e1684e05d5da10929191537759981907610a
developer <developer@Neik-no-Mac-mini.local>
2014-11-26 17:08:18 +0700

    init project

--- NinjaGame/GameScene.swift
+++ NinjaGame/GameScene.swift
@@ -9,37 +9,103 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var monsters: [SKSpriteNode] = [SKSpriteNode]()
+    var numberDestroy: Int = Int()
+    var scoreMax = 10
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+        /* thay đổi màu của màn hình thành màu trắng */
+        backgroundColor = SKColor.whiteColor()
+        self.numberDestroy = 0
+        // thêm bóng ma vào màn hình , sau mỗi giây ta thêm vào một bóng ma
+        runAction(SKAction.repeatActionForever(
+            SKAction.sequence([
+                SKAction.runBlock(addMonster),
+                SKAction.waitForDuration(1.0)
+                ])
+            ))
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
-        
         for touch: AnyObject in touches {
             let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+            checkTouch(location)
         }
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
+    
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    
+    func random(#min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    
+    func checkTouch(position: CGPoint) {
+        var monstersRemove: [SKSpriteNode] = [SKSpriteNode]()
+        for monster in monsters {
+            var mP: CGPoint = CGPoint(x: monster.position.x, y: monster.position.y)
+//            println(mP.x )
+            let xDist: CGFloat = (position.x - monster.position.x)
+            let yDist: CGFloat = (position.y - monster.position.y)
+            let distance: CGFloat = sqrt((xDist * xDist) + (yDist * yDist))
+            println(monster.size.width)
+            if (distance < monster.size.width) {
+                monstersRemove.append(monster)
+                addPraticle(position)
+            }
+        }
+        
+        for monster in monstersRemove {
+          //  monsters.removeAtIndex(indexOfAccessibilityElement(monster))
+            monster.removeFromParent()
+            numberDestroy = numberDestroy + 1
+            if (numberDestroy > scoreMax) {
+                let reveal = SKTransition.flipHorizontalWithDuration(0.5)
+                let gameOverScene = GameOverScene(size: self.size, won: true)
+                self.view?.presentScene(gameOverScene, transition: reveal)
+            }
+
+        }
+    }
+    
+    
+    func addMonster() {
+        // tạo một sprite tên là monster
+        let monster = SKSpriteNode(imageNamed: "monster")
+        monsters.append(monster)
+        // giới hạn toạ độ Y của monster
+        let actualY = random(min: monster.size.height, max: size.height - monster.size.height)
+        
+        // đặt toạ độ cho monster ở phía sau bên phải màn hình
+        monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
+        
+        // thêm monster vào màn hình game
+        addChild(monster)
+        
+        // thời gian di chuyển của monster
+        let actualDuration = random(min: CGFloat(4.0), max: CGFloat(5.0))
+        
+        // hành động di chuyển từ phải qua trái
+        let actionMove = SKAction.moveTo(CGPoint(x: -monster.size.width/2, y: actualY), duration: NSTimeInterval(actualDuration))
+        // xoá monster sau khi nó di chuyển xong
+        let actionMoveDone = SKAction.removeFromParent()
+        // kiểm tra nếu monster di chuyển qua mép trái màn hình thì kết thúc game
+        let loseAction = SKAction.runBlock() {
+            let reveal = SKTransition.flipHorizontalWithDuration(0.5)
+            let gameOverScene = GameOverScene(size: self.size, won: false)
+            self.view?.presentScene(gameOverScene, transition: reveal)
+        }
+        monster.runAction(SKAction.sequence([actionMove, loseAction, actionMoveDone]))
+    }
+    
+    func addPraticle(p:CGPoint)
+    {
+        
+    }
+
 }
